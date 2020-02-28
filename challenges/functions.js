4db51b0e20cc7a3b61a4f7b30dec92d8e12c5974
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-02-28 11:09:48 -0600

    completed all questions, along with their console.log showing the correct results, completed stretch question on object-arrays.js and classes.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,14 +6,26 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb){
+  return cb(a,b); 
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1,num2){
+  return num1 + num2; 
+}
 
+function multiply(num1,num2){
+  return num1 * num2; 
+}
+
+function greeting(first,last){
+  return `Hello ${first} ${last}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
@@ -27,6 +39,8 @@
 
 // Explanation: 
 
+
+/* nestedFunction has access to the variable internal because this is an important feature of closures. The important thing is that on the function scope, variables declared here are able to be passed down into nested functions, therefore since nestedFunction is a function inside of the myFunction function, it has access to any variables declared on myFunction's function scope. However, a unique exception to closures is that you can not access variables up to other functions, meaning if nestedFunction had a variable declared inside of its function scope then myFunction would not have access to it. */
 
 const external = "I'm outside the function";
 
